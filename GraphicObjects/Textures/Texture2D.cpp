fb7c44db5b6b6474ef854abe5cf50c733729d634
Luke <lukas.zaborsky@gmail.com>
2024-05-06 10:39:26 +0200

    Add base texture support

--- GraphicObjects/Textures/Texture2D.cpp
+++ GraphicObjects/Textures/Texture2D.cpp
@@ -8,14 +8,16 @@
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb_image.h"
 
-Texture2D::Texture2D(uint32_t width, uint32_t height)
+void Texture2D::Initialize(uint32_t width, uint32_t height)
 {
-    SetDescriptor(width, height);
+    SetDefaultDescriptor<2>();
+    SetDefaultViewDescriptor<2>();
 
+    m_desc.mipLevelCount = 1;
 	m_desc.format = WGPUTextureFormat_RGBA8Unorm;
 	m_desc.usage = WGPUTextureUsage_TextureBinding | WGPUTextureUsage_CopyDst;
 
-    m_texture = wgpuDeviceCreateTexture(Device::Get(), &m_desc);
+    Init(width, height, 1);
 
     std::vector<uint8_t> pixels(4 * m_desc.size.width * m_desc.size.height);
     for (uint32_t i = 0; i < m_desc.size.width; i += 2) {
@@ -28,86 +30,27 @@ Texture2D::Texture2D(uint32_t width, uint32_t height)
         }
     }
 
-    WGPUImageCopyTexture destination = {};
-    destination.texture = m_texture;
-    destination.mipLevel = 0;
-    destination.origin = { 0, 0, 0 }; // equivalent of the offset argument of Queue::writeBuffer
-    destination.aspect = WGPUTextureAspect_All; // only relevant for depth/Stencil textures
-
-    WGPUTextureDataLayout source = {};
-
-    source.offset = 0;
-    source.bytesPerRow = 4 * m_desc.size.width;
-    source.rowsPerImage = m_desc.size.height;
-
-    wgpuQueueWriteTexture(Device::GetQueue(), &destination, pixels.data(), pixels.size(), &source, &m_desc.size);
-
+    Write(&pixels[0], { 0,0,0 }, { width, height, 1 }, 4, 0);
     CreateView();
-    
 }
 
-Texture2D::Texture2D(const std::filesystem::path& path)
+void Texture2D::Initialize(const std::filesystem::path& path)
 {
     int width, height, channels;
     stbi_set_flip_vertically_on_load(1);
     char* data = (char*)stbi_load(((path).generic_string()).data(), &width, &height, &channels, 4);
 
-    SetDescriptor(width, height);
+    SetDefaultDescriptor<2>();
+    SetDefaultViewDescriptor<2>();
 
     m_desc.format = WGPUTextureFormat_RGBA8Unorm;
     m_desc.usage = WGPUTextureUsage_TextureBinding | WGPUTextureUsage_CopyDst;
 
-    m_texture = wgpuDeviceCreateTexture(Device::Get(), &m_desc);
+    Init(width, height, 1);
 
-    WGPUImageCopyTexture destination = {};
-    destination.texture = m_texture;
-    destination.mipLevel = 0;
-    destination.origin = { 0, 0, 0 }; // equivalent of the offset argument of Queue::writeBuffer
-    destination.aspect = WGPUTextureAspect_All; // only relevant for depth/Stencil textures
-
-    WGPUTextureDataLayout source = {};
-
-    source.offset = 0;
-    source.bytesPerRow = 4 * m_desc.size.width;
-    source.rowsPerImage = m_desc.size.height;
-
-    wgpuQueueWriteTexture(Device::GetQueue(), &destination, data, width * height * channels, &source, &m_desc.size);
+    Write(data, { 0,0,0 }, { width, height, 1 }, channels);
 
     CreateView();
     stbi_image_free(data);
 }
 
-Texture2D::~Texture2D()
-{
-    wgpuTextureDestroy(m_texture);
-    wgpuTextureRelease(m_texture);
-    if (m_view) wgpuTextureViewRelease(m_view);
-}
-
-void Texture2D::SetDescriptor(uint32_t width, uint32_t height)
-{
-    m_desc.nextInChain = nullptr;
-    m_desc.dimension = WGPUTextureDimension_2D;
-    m_desc.size = { width, height, 1 };
-
-    m_desc.mipLevelCount = 1;
-    m_desc.sampleCount = 1;
-
-    m_desc.viewFormatCount = 0;
-    m_desc.viewFormats = nullptr;
-}
-
-void Texture2D::CreateView()
-{
-    if (m_view) wgpuTextureViewRelease(m_view);
-
-    WGPUTextureViewDescriptor textureViewDesc = {};
-    textureViewDesc.aspect = WGPUTextureAspect_All;
-    textureViewDesc.baseArrayLayer = 0;
-    textureViewDesc.arrayLayerCount = 1;
-    textureViewDesc.baseMipLevel = 0;
-    textureViewDesc.mipLevelCount = 1;
-    textureViewDesc.dimension = WGPUTextureViewDimension_2D;
-    textureViewDesc.format = m_desc.format;
-    m_view = wgpuTextureCreateView(m_texture, &textureViewDesc);
-}
--- GraphicObjects/Textures/Texture2D.h
+++ GraphicObjects/Textures/Texture2D.h
@@ -1,30 +1,18 @@
 #pragma once
 #include "webgpu/webgpu.h"
 #include <filesystem>
-#include "Uncopyable.h"
+#include "Texture.h"
 
 
 
-class Texture2D : Uncopyable
+class Texture2D : public Texture
 {
 public:
-	Texture2D();
-	Texture2D(uint32_t width, uint32_t height);
-	Texture2D(const std::filesystem::path& path);
-	~Texture2D();
+	Texture2D() {};
 
+	void Initialize(uint32_t width, uint32_t height);
 
-	WGPUTextureView GetView() const {
-		return m_view;
-	}
-
-private:
-	void SetDescriptor(uint32_t width, uint32_t height);
-	void CreateView();
-
-	WGPUTextureDescriptor m_desc = {};
-	WGPUTexture m_texture = nullptr;
-	WGPUTextureView m_view = nullptr;
+	void Initialize(const std::filesystem::path& path);
 };
 
 
