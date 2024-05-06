a895d5201ecf7e1e683a31856eec3900f6067cb6
Luke <lukas.zaborsky@gmail.com>
2024-05-06 16:50:50 +0200

    add methods for manipulating 2d textures

--- GraphicObjects/Textures/Texture2D.h
+++ GraphicObjects/Textures/Texture2D.h
@@ -10,9 +10,19 @@ class Texture2D : public Texture
 public:
 	Texture2D() {};
 
-	void Initialize(uint32_t width, uint32_t height);
+	void Init(uint32_t width, uint32_t height);
 
-	void Initialize(const std::filesystem::path& path);
+	void Init(const std::filesystem::path& path);
+
+	inline void Write(void* bytes, const glm::vec<2, uint32_t>& offset, const glm::vec<2, uint32_t>& size, size_t pixelSize, uint32_t miplevel = 0);
 };
 
+#pragma region Definitions
+
+inline void Texture2D::Write(void* bytes, const glm::vec<2, uint32_t>& offset, const glm::vec<2, uint32_t>& size, size_t pixelSize, uint32_t miplevel)
+{
+	i_Write(bytes, { offset, 1 }, { size, 1 }, pixelSize, miplevel);
+}
+
+#pragma endregion
 
